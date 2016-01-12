8acba65edb37580ea2a934923f1ce26db1b13fe7
Tomio Mizoroki <tmizoroki@gmail.com>
2016-01-12 03:04:15 -0800

    Change to node module format

--- prime.js
+++ prime.js
@@ -1,41 +1,32 @@
-var primes = (function primeModule() {
-
-  // Simple function to test if the input is a prime number
-  function isPrime(num) {
-    var result = true;
-    if (num < 2 || (num > 2 && num % 2 === 0)) {
-      result = false;
-    } else {
-      for (var i = 2; i <= Math.sqrt(num); i++) {
-        if (num % i === 0) {
-          result = false;
-          break;
-        }
+// Simple function to test if the input is a prime number
+exports.isPrime = function(num) {
+  var result = true;
+  if (num < 2 || (num > 2 && num % 2 === 0)) {
+    result = false;
+  } else {
+    for (var i = 2; i <= Math.sqrt(num); i++) {
+      if (num % i === 0) {
+        result = false;
+        break;
       }
     }
-    return result;
   }
+  return result;
+};
 
-  // Returns an array of the first N primes
-  function findPrimes(n) {
-    var primes = [];
-    var counter = 0;
-    var current = 2;
+// Returns an array of the first N primes
+exports.findPrimes = function(n) {
+  var primes = [];
+  var counter = 0;
+  var current = 2;
 
-    while (counter < n) {
-      if (isPrime(current)) {
-        primes.push(current);
-        counter++;
-      }
-      current++;
+  while (counter < n) {
+    if (isPrime(current)) {
+      primes.push(current);
+      counter++;
     }
-
-    return primes;
+    current++;
   }
 
-  return {
-    isPrime: isPrime,
-    findPrimes: findPrimes
-  }
-  
-})();
+  return primes;
+};
