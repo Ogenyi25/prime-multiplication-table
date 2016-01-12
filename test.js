96b2bc81e59607bdd91fe7f450bff476396093b3
Tomio Mizoroki <tmizoroki@gmail.com>
2016-01-12 04:55:10 -0800

    Fix assertions that were written incorrectly

--- test.js
+++ test.js
@@ -1,34 +1,33 @@
 var assert = require('assert');
+var primes = require('./prime.js');
+var grid = require('./table.js')
 
-describe('Prime Module', function() {
+describe('Prime Module', function () {
   
   describe('isPrime function', function () {
     
-    it('should throw an error if it is not given a positive integer greater than 1', function () {
-      assert.throws(isPrime, Error);
-      assert.throws(function() { primes.isPrime(0) }, Error);
-      assert.throws(function() { primes.isPrime(1) }, Error);
-      assert.throws(function() { primes.isPrime(-1) }, Error);
-      assert.throws(function() { primes.isPrime(3.1) }, Error);
-      assert.throws(function() { primes.isPrime('a') }, Error);
-      assert.throws(function() { primes.isPrime(true) }, Error);
-      assert.throws(function() { primes.isPrime(undefined) }, Error);
+    it('should throw an error if it is not given a positive integer', function () {
+      assert.throws(primes.isPrime, Error, "You must input an integer");
+      assert.throws(function () { primes.isPrime(3.1) });
+      assert.throws(function () { primes.isPrime('a') });
+      assert.throws(function () { primes.isPrime(true) });
+      assert.throws(function () { primes.isPrime(undefined) });
     });
     
     it('should be able to test primality', function () {
-      assert.equal(function() { primes.isPrime(2) }, true);
-      assert.equal(function() { primes.isPrime(3) }, true);
-      assert.equal(function() { primes.isPrime(4) }, false);
-      assert.equal(function() { primes.isPrime(5) }, true);
-      assert.equal(function() { primes.isPrime(7) }, true);
-      assert.equal(function() { primes.isPrime(9) }, true);
-      assert.equal(function() { primes.isPrime(12) }, false);
-      assert.equal(function() { primes.isPrime(13) }, true);
-      assert.equal(function() { primes.isPrime(15) }, false);
-      assert.equal(function() { primes.isPrime(19) }, true);
-      assert.equal(function() { primes.isPrime(21) }, false);
-      assert.equal(function() { primes.isPrime(23) }, true);
-      assert.equal(function() { primes.isPrime(25) }, false);
+      assert.equal(primes.isPrime(2), true);
+      assert.equal(primes.isPrime(3), true);
+      assert.equal(primes.isPrime(4), false);
+      assert.equal(primes.isPrime(5), true);
+      assert.equal(primes.isPrime(7), true);
+      assert.equal(primes.isPrime(9), false);
+      assert.equal(primes.isPrime(12), false);
+      assert.equal(primes.isPrime(13), true);
+      assert.equal(primes.isPrime(15), false);
+      assert.equal(primes.isPrime(19), true);
+      assert.equal(primes.isPrime(21), false);
+      assert.equal(primes.isPrime(23), true);
+      assert.equal(primes.isPrime(25), false);
     });
 
   });
@@ -36,20 +35,18 @@ describe('Prime Module', function() {
   describe('First N primes function', function () {
     
     it('should return 10 numbers if nothing is passed in', function () {
-      assert.equal(function() { primes.findPrimes().length }, 10);
+      assert.equal(primes.findPrimes().length, 10);
     });
 
     it('should return N numbers if N is passed in', function () {
-      assert.equal(function() { primes.findPrimes(12).length }, 12);
-      assert.equal(function() { primes.findPrimes(8).length }, 8);
+      assert.equal(primes.findPrimes(12).length, 12);
+      assert.equal(primes.findPrimes(8).length, 8);
     });
 
     it('should throw an error if a positive integer is not passed in', function () {
-      assert.throws(function() { primes.findPrimes(-1) }, Error);
-      assert.throws(function() { primes.findPrimes(0) }, Error);
-      assert.throws(function() { primes.findPrimes('a') }, Error);
-      assert.throws(function() { primes.findPrimes(true) }, Error);
-      assert.throws(function() { primes.findPrimes(undefined) }, Error);
+      assert.throws(function () { primes.findPrimes(-1) });
+      assert.throws(function () { primes.findPrimes('a') });
+      assert.throws(function () { primes.findPrimes(true) });
     });
 
   });
@@ -58,6 +55,8 @@ describe('Prime Module', function() {
 
 describe('Grid Module', function () {
 
-  it('should display tabular data');
+  it('should display tabular data', function () {
+    assert.equal(grid.drawTable([[1,10,100,],[20,200,2000,],[300,30,300]]), "  1  10  100\n 20 200 2000\n300  30  300")
+  });
 
 });
