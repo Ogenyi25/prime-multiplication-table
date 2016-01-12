b497f3e865ecf4fc6a719814a1032ec8c4c13b29
Tomio Mizoroki <tmizoroki@gmail.com>
2016-01-12 04:54:29 -0800

    Bad input throws an error

--- prime.js
+++ prime.js
@@ -1,5 +1,10 @@
 // Simple function to test if the input is a prime number
 exports.isPrime = function(num) {
+
+  if (typeof num !== 'number' || num % 1 !== 0) {
+    throw new Error("You must input an integer")
+  }
+
   var result = true;
   if (num < 2 || (num > 2 && num % 2 === 0)) {
     result = false;
@@ -16,12 +21,17 @@ exports.isPrime = function(num) {
 
 // Returns an array of the first N primes
 exports.findPrimes = function(n) {
+  n = n || 10;
   var primes = [];
   var counter = 0;
   var current = 2;
 
+  if (typeof n !== 'number' || n <= 0 || n % 1 !== 0) {
+    throw new Error("You must input a positive integer")
+  }
+
   while (counter < n) {
-    if (isPrime(current)) {
+    if (exports.isPrime(current)) {
       primes.push(current);
       counter++;
     }
